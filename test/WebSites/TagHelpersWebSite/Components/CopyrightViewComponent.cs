529e273a1fbbf30cd84a64e9a96047e115da33fe
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-10-18 15:00:40 -0700

    Added a functional test for ViewComponent tag helpers

---
+++ test/WebSites/TagHelpersWebSite/Components/CopyrightViewComponent.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelpersWebSite
+{
+    public class CopyrightViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string website, int year)
+        {
+            var dict = new Dictionary<string, object>();
+            dict["website"] = website;
+            dict["year"] = year;
+
+            return View("Copyright", dict);
+        }
+    }
+}
